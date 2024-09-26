148e2354d1d21b9de733a224a83407fc0aa40362
Marat <pmaratd@gmail.com>
2024-09-26 12:00:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
-def trapezoid_area(a, b, c, d):
-    
-    p = (a + b + c + d) / 2
-    
-    area = (p * (p - a) * (p - b) * (p - c) * (p - d)) ** 0.5
-    
-    return area
-
 a = 5 
 b = 7
 c = 5
 d = 3 
+    
+p = (a + b + c + d) / 2
+    
+area = (p * (p - a) * (p - b) * (p - c) * (p - d)) ** 0.5
 
-area = trapezoid_area(a, b, c, d)
 print(f"Площадь трапеции: {area:.2f}")
